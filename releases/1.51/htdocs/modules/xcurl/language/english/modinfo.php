54aa9426c4b41cc9d48d33bb24bebae2a2f03eda
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-08-11 12:46:10 +0000

    Removing spaces from "define", which is required for Transifex per DCrusader

--- releases/1.51/htdocs/modules/xcurl/language/english/modinfo.php
+++ releases/1.51/htdocs/modules/xcurl/language/english/modinfo.php
@@ -24,21 +24,21 @@
 	define('_XC_WSDLUNSUCCESSFUL','Database Updated - Error Compiling WSDL');
 	define('_XC_DATABASEUPDATED','Database Updated');						
 	
-	define('_XC_SECONDS', 'Function lock out time');
-	define('_XC_SECONDS_DESC', 'Period of time for locked function to be invocated on incorrect username and password details!!');
+	define('_XC_SECONDS','Function lock out time');
+	define('_XC_SECONDS_DESC','Period of time for locked function to be invocated on incorrect username and password details!!');
 
-	define('_XC_SECONDSCACHE', 'Lockout cache stored for');
-	define('_XC_SECONDSCACHE_DESC', 'Period of time for cache function to be invocated!');
+	define('_XC_SECONDSCACHE','Lockout cache stored for');
+	define('_XC_SECONDSCACHE_DESC','Period of time for cache function to be invocated!');
 		
-	define('_XC_SECONDS_3600', '1 Hour');
-	define('_XC_SECONDS_1800', '30 minutes');
-	define('_XC_SECONDS_1200', '20 minutes');
-	define('_XC_SECONDS_600', '10 minutes');
-	define('_XC_SECONDS_300', '5 Minutes');
-	define('_XC_SECONDS_180', '3 Minutes');
-	define('_XC_SECONDS_60', '1 Minute');
-	define('_XC_SECONDS_30', '30 Seconds');	
+	define('_XC_SECONDS_3600','1 Hour');
+	define('_XC_SECONDS_1800','30 minutes');
+	define('_XC_SECONDS_1200','20 minutes');
+	define('_XC_SECONDS_600','10 minutes');
+	define('_XC_SECONDS_300','5 Minutes');
+	define('_XC_SECONDS_180','3 Minutes');
+	define('_XC_SECONDS_60','1 Minute');
+	define('_XC_SECONDS_30','30 Seconds');	
 	
-	define('_XC_USERANDOMLOCK', 'Random seconds seed (maximum value)');
-	define('_XC_USERANDOMLOCK_DESC', 'You should not set this above the maximum function lockout time!');
+	define('_XC_USERANDOMLOCK','Random seconds seed (maximum value)');
+	define('_XC_USERANDOMLOCK_DESC','You should not set this above the maximum function lockout time!');
 ?>
